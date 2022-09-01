4d68ff2cf549626a86c245f79dd302cb7579732f
Rodrigo <rlvenancio@gmail.com>
2022-09-01 18:14:19 -0300

    Add conexão com MySQL
    Adiciona conexão com banco de dados MySQL por meio dos pacotes pymysql - "pip3 install pymysql" e dotenv - "pip install python-dotenv" para trabalhar com variáveis de ambiente. Efetiva migração dos dados por usando o comando "python manage.py migrate"

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('nome_bd'),
+        'USER': os.getenv('usuario_bd'),
+        'PASSWORD': os.getenv('senha_bd'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
