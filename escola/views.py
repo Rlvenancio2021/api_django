a86f178e41569812b41d24aa21b5f5cdba07dc83
Rodrigo <rlvenancio@gmail.com>
2022-09-02 16:53:01 -0300

    Cria ViewSet
    Cria estrutura para lidar com as ViewSet tanto dos Alunos quanto dos Cursos

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,18 @@
-from multiprocessing.dummy import JoinableQueue
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializers import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Rodrigo'}
-        return JsonResponse(aluno)
+'''
+A estrutura Rest permite incluir uma abstração para lidar com "VIEWSET",
+que permite pensar na modelagem de negócio e como a aplicação esta se desenvolvendo.
+'''
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.obects.all()
+    serializer_class = CursoSerializer
